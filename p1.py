85dbd04bb87724ae8f4a1159871807cf1316bef4
Aditya Raj Singh <42737287+AdityaRajSingh@users.noreply.github.com>
2019-08-17 21:50:42 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -8,8 +8,8 @@ search_form_username = None
 while search_form_password == None or search_form_username == None:
     search_form_username = webbrowser.find_element_by_name('frmHTTPClientLogin').find_element_by_class_name('maindiv').find_element_by_class_name('datablock').find_element_by_class_name('tablecss').find_element_by_id('usernametxt').find_element_by_name('username')
     search_form_password = webbrowser.find_element_by_name('frmHTTPClientLogin').find_element_by_class_name('maindiv').find_element_by_class_name('datablock').find_element_by_class_name('tablecss').find_element_by_name('password')
-search_form_username.send_keys(open(r"Login/username.txt",'r').read())
-search_form_password.send_keys(open(r"Login/password.txt",'r').read())
+search_form_username.send_keys(open(r"username.txt",'r').read())
+search_form_password.send_keys(open(r"password.txt",'r').read())
 search_form_submit = webbrowser.find_element_by_name('frmHTTPClientLogin').find_element_by_class_name('maindiv').find_element_by_class_name('datablock').find_element_by_class_name('tablecss').find_element_by_name('btnSubmit')
 search_form_submit.click()
 webbrowser.close()
